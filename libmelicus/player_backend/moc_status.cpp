a069e20499fd96a07754a97753a08c52606a4156
aram <leandrojavr@gmail.com>
2020-10-13 16:21:59 -0300

    Implementation of a get tag function for mocp --info

--- libmelicus/player_backend/moc_status.cpp
+++ libmelicus/player_backend/moc_status.cpp
@@ -1,19 +1,3 @@
-/* State: PAUSE
-File:
-Title:
-Artist:
-SongTitle:
-Album:
-TotalTime:
-TimeLeft:
-TotalSec:
-CurrentTime:
-CurrentSec:
-Bitrate: 128kbps
-AvgBitrate: 128kbps
-Rate: 44kHz
-*/
-
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
@@ -24,6 +8,8 @@ Rate: 44kHz
 #include <vector>
 #include <string>
 #include <iostream>
+
+//#include <../audio_status.hpp>
 
 static bool get_line(int fd, std::string *s) {
     while (true) {
@@ -41,7 +27,7 @@ static bool get_line(int fd, std::string *s) {
     }
 }
 
-std::vector<std::string> get_moc_status() {
+std::vector<std::string> mocp_info() {
     int pipe_fd[2]; // Read from 0 write to 1
     int pipe_err[2]; // Pipe to read stderr
     pipe(pipe_fd);
@@ -78,10 +64,50 @@ std::vector<std::string> get_moc_status() {
     }
 }
 
-int main() {
-    auto v = get_moc_status();
+/* State: PAUSE
+File:
+Title:
+Artist:
+SongTitle:
+Album:
+TotalTime:
+TimeLeft:
+TotalSec:
+CurrentTime:
+CurrentSec:
+Bitrate: 128kbps
+AvgBitrate: 128kbps
+Rate: 44kHz
+*/
 
-    for (auto &vv : v) {
+static std::string find_tag(std::vector<std::string> &info, std::string tag) {
+    std::string tag_info{""};
+    for (auto &v : info) {
+        if (v.size() < tag.size())
+            continue;
+        auto [p1, p2] = std::mismatch(tag.begin(), tag.end(), v.begin());
+        if (p1 != tag.end()) // found tag
+            continue;
+        while (p2 != v.end()) {
+            tag_info.push_back(*p2);
+            p2++;
+        }
+    }
+
+    return tag_info;
+}
+
+void moc_status() {
+    std::vector<std::string> info = mocp_info();
+
+    for (auto &vv : info) {
         std::cout << vv << std::endl;
     }
+
+    std::cout << find_tag(info, "File: ") << std::endl;
+    std::cout << find_tag(info, "Title: ") << std::endl;
+}
+
+int main() {
+    moc_status();
 }
